9e18b31e1ce689b221203ff32adf2f553b0949e0
HongzCloud <jihong21511@gmail.com>
2022-02-26 19:31:27 +0900

    ✨ [#1] Dishes 모델 생성

---
+++ SideDishWithRx/SideDishWithRx/Domain/Entity/Dishes.swift
@@ -0,0 +1,44 @@
+//
+//  Dishes.swift
+//  SideDishWithRx
+//
+//  Created by 오킹 on 2022/02/26.
+//
+
+import Foundation
+
+// MARK: - Dishes
+
+struct Dishes: Codable {
+    let statusCode: Int
+    let body: [Dish]
+}
+
+// MARK: - Dish
+
+struct Dish: Codable {
+    let detailHash: String
+    let image: String
+    let alt: String
+    let deliveryType: [DeliveryType]
+    let title, bodyDescription: String
+    let nPrice: String?
+    let sPrice: String
+    let badge: [String]?
+
+    enum CodingKeys: String, CodingKey {
+        case detailHash = "detail_hash"
+        case image, alt
+        case deliveryType = "delivery_type"
+        case title
+        case bodyDescription = "description"
+        case nPrice = "n_price"
+        case sPrice = "s_price"
+        case badge
+    }
+}
+
+enum DeliveryType: String, Codable {
+    case dawnDelivery = "새벽배송"
+    case nationalDelivery = "전국택배"
+}
